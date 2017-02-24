602afb9ef4d0eac147e2b6bc05c5120692f2ac83
Shikhar Bakhda <ssb2189@columbia.edu>
2017-02-23 23:16:21 -0500

    changed stuff

--- plot/models.py
+++ plot/models.py
@@ -8,8 +8,8 @@ class MonthlyWeatherByCity(models.Model):
     month = models.IntegerField()
     boston_temp = models.DecimalField(max_digits=5, decimal_places=1)
     houston_temp = models.DecimalField(max_digits=5, decimal_places=1)
-    # new_york_temp = models.DecimalField(max_digits=5, decimal_places=1)
-    # san_franciso_temp = models.DecimalField(max_digits=5, decimal_places=1)
+    new_york_temp = models.DecimalField(max_digits=5, decimal_places=1)
+    san_franciso_temp = models.DecimalField(max_digits=5, decimal_places=1)
 
 
 class MonthlyWeatherSeattle(models.Model):
