cd32590036a0b664cfa71a46b7cd9a28db710956
Shikhar Bakhda <ssb2189@columbia.edu>
2017-02-25 17:00:40 -0500

    added highstock code

--- plot/views.py
+++ plot/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, render_to_response
 from chartit import DataPool, Chart
 from .models import MonthlyWeatherByCity
+import json, urllib
+from django.http import JsonResponse, HttpResponse
+
 
 # Create your views here.
 def weather_chart_view(request):
@@ -37,3 +40,34 @@ def weather_chart_view(request):
 
     #Step 3: Send the chart object to the template.
     return render_to_response('plot/plot.html',{'weatherchart': cht})
+
+def plot(request) :
+    url = 'https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?'
+    # response = JsonResponse({1: 5})
+    # response = urllib.urlopen(url)
+    # data = json.loads(response.read())
+    # js = response.content
+    # 'https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?'
+    jstring = '{"chart_data": {"dates": [1396310400, 1396396800, 1396483200, 1396569600], "values": [2, 3, 2, 4]}}'
+    js = json.loads(jstring)
+    return render(request, 'plot/index.html', {'js':js})
+    # return HttpResponse(json.dumps(json.loads(jstring)))
+
+def disp(request) :
+    if request.method == "GET":
+        print("got")
+
+        data = {
+            "chart_data": {
+                "dates": [1396310400, 1396396800, 1396483200, 1396569600],
+                "values": [2, 3, 2, 4]
+            }
+        }
+        data = [
+        [1267401600000, 29.86],
+        [1267488000000, 29.84],
+        [1267574400000, 29.90],
+        [1267660800000, 30.10]
+        ]
+
+    return JsonResponse(data)
--- plotDjango/urls.py
+++ plotDjango/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from plot import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('plot.urls')),
+    url(r'^$', views.plot, name='plot'),
+    url(r'^getreq/$', views.disp)
 ]
